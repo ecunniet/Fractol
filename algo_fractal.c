5757fa0c1cd9e13259c16aee1673e0d115af6340
ecunniet <ecunniet@e2r1p6.42.fr>
2017-03-03 18:01:19 +0100

    hey

--- algo_fractal.c
+++ algo_fractal.c
@@ -15,7 +15,7 @@ void	ft_pixel_put_image(t_env *list, int x, int y)
 		*(((int*)list->adi) + tmp) = 0xff23ff;
 }
 
-void	ft_mandelbrot(t_env *list)
+int	ft_mandelbrot(t_env *list)
 {
 // on définit la zone que l'on dessine. Ici, la fractale en entière
 	double	x1 = -2.1;
@@ -24,7 +24,7 @@ void	ft_mandelbrot(t_env *list)
 	double	y2 = 1.2;
 	double	image_x = 400;
 	double	image_y = 370;
-	double	iteration_max = 20000;
+	double	iteration_max = 500;
 	double	zoom_x = image_x/(x2 - x1);
 	double	zoom_y = image_y/(y2 - y1);
 	double	x = 0;
@@ -59,6 +59,41 @@ void	ft_mandelbrot(t_env *list)
 		x++;
 	}
 	mlx_put_image_to_window(list->mlx, list->win, list->img_ptr, 0, 0);
+	return (0);
+}
+
+int			ft_key_funct(int keycode, t_env *list)
+{
+	list->zoom = (keycode == 67) ? (list->zoom + 0.2) : list->zoom;
+	list->zoom = (keycode == 75 && list->zoom > 0.3) ? (list->zoom - 0.2)
+	: list->zoom;
+	if (keycode == 53)
+		exit(EXIT_SUCCESS);
+	return (0);
+}
+
+int			ft_mouse_funct(int button, int x, int y, t_env *list)
+{
+	printf("button = %d et x = %d et y = %d\n", button, x, y);
+	if (button == 1)
+	{
+		list->center_x = x;
+		list->center_y = y;
+	}
+	if (button == 2)
+	{
+		list->b_x = 0;
+		list->b_y = 0;
+		list->b_z = 0;
+		list->angle_x = 15;
+		list->angle_y = 25;
+		list->angle_z = 0;
+		list->center_x = -1;
+		list->center_y = -1;
+		list->zoom = 1;
+		list->rainbow = 0;
+	}
+	return (0);
 }
 
 int		ft_draw_pix(t_env *list)
@@ -71,6 +106,9 @@ int		ft_draw_pix(t_env *list)
 	if (list->i == 2)
 		ft_mandelbrot(list);
 	mlx_put_image_to_window(list->mlx, list->win, list->img_ptr, 0, 0);
+	mlx_key_hook(list->win, ft_key_funct, list);
+	mlx_mouse_hook(list->win, ft_mouse_funct, list);
+	mlx_loop_hook(list->mlx, &ft_mandelbrot, list);
 	mlx_loop(list->mlx);
 	return (0);
 }
